cda05ec7a372ebc04b067fabdd22003fad1b8e0a
anish <anish22072003@gmail.com>
2024-04-21 21:59:31 +0530

    v1

---
+++ main2.py
@@ -0,0 +1,81 @@
+import cv2
+import dlib
+import pyttsx3
+import imutils
+from imutils import face_utils
+from scipy.spatial import distance as dist
+
+
+
+engine = pyttsx3.init()
+# Function to calculate eye aspect ratio (EAR)
+def eye_aspect_ratio(eye):
+    # Calculate the Euclidean distances between the vertical eye landmarks
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    # Calculate the Euclidean distance between the horizontal eye landmarks
+    C = dist.euclidean(eye[0], eye[3])
+    # Compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+FACIAL_LANDMARK_PREDICTOR = "shape_predictor_68_face_landmarks.dat"
+MINIMUM_EAR = 0.25
+MAXIMUM_FRAME_COUNT = 60
+
+faceDetector = dlib.get_frontal_face_detector()
+landmarkFinder = dlib.shape_predictor(FACIAL_LANDMARK_PREDICTOR)
+webcamFeed = cv2.VideoCapture(0)
+
+(leftEyeStart, leftEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rightEyeStart, rightEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+EYE_CLOSED_COUNTER = 0
+
+try:
+    while True:
+        (status, image) = webcamFeed.read()
+        image = imutils.resize(image, width=800)
+        grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        faces = faceDetector(grayImage, 0)
+
+        for face in faces:
+            faceLandmarks = landmarkFinder(grayImage, face)
+            faceLandmarks = face_utils.shape_to_np(faceLandmarks)
+
+            leftEye = faceLandmarks[leftEyeStart:leftEyeEnd]
+            rightEye = faceLandmarks[rightEyeStart:rightEyeEnd]
+
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+
+            ear = (leftEAR + rightEAR) / 2.0
+
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+
+            cv2.drawContours(image, [leftEyeHull], -1, (255, 0, 0), 2)
+            cv2.drawContours(image, [rightEyeHull], -1, (255, 0, 0), 2)
+
+            if ear < MINIMUM_EAR:
+                EYE_CLOSED_COUNTER += 1
+            else:
+                EYE_CLOSED_COUNTER = 0
+
+            cv2.putText(image, "EAR: {:.2f}".format(ear), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+            if EYE_CLOSED_COUNTER >= MAXIMUM_FRAME_COUNT:
+                cv2.putText(image, "Drowsiness", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                engine.say("Alert!!!! WAKE UP DUDE")
+                engine.runAndWait()
+
+        cv2.imshow("Frame", image)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):  # Press 'q' key to exit
+            break
+except Exception as e:
+    print("Error:", e)
+
+webcamFeed.release()
+cv2.destroyAllWindows()
